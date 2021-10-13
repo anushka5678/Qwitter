d806c65cfe66e6931804cf658532a5a37bfa35f9
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2021-10-13 17:55:26 +0530

    Add files via upload

---
+++ qwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVHOlxMiYZv2HGvX2dz8-mxtXLD-Vc_Ts",
+    authDomain: "qwitter-9e273.firebaseapp.com",
+    databaseURL: "https://qwitter-9e273-default-rtdb.firebaseio.com",
+    projectId: "qwitter-9e273",
+    storageBucket: "qwitter-9e273.appspot.com",
+    messagingSenderId: "1027295148954",
+    appId: "1:1027295148954:web:a4c6314f0ed5bacacc5823",
+    measurementId: "G-PPJQK0JGHR"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
