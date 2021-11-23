893396caf517e077b04d032d8fe5b89cb34543b0
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2021-11-23 17:02:40 +0530

    Add files via upload

--- qwitter_room.js
+++ qwitter_room.js
@@ -11,4 +11,31 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  user_name= localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML=user_name;
+
+  function addRoom(){
+    room_name= document.getElementById("room_name").value ;
+    localStorage.setItem("room_name", room_name);
+    firebase.database.ref("/").child("room_name").update({
+      pupose: "adding room name"
+    });
+    window.location= "qwitter_page.html";
+  }
   
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+console.log("Room Name - " + Room_names);
+row = "<div class='room_name' id=" +Room_names+"onclick=redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+document.getElementById("output").innerHTML += row;
+//End code
+});});}
+getData();
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("room_name",name);
+  window.location="qwitter_page.html";
+}
